a257e52f838a4a386e3efd7c1ac6132ab17ad59d
Landl3ss <wnoland90@gmail.com>
2022-04-20 13:08:55 -0700

    starting over

---
+++ original_commerce/auctions/models.py
@@ -0,0 +1,31 @@
+from tkinter import Widget
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=25, unique=True)
+    email = models.EmailField(max_length=25, unique=True)
+    password = models.CharField(max_length=25)
+    watchlist = []
+
+class Listings(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, default=None, related_name='seller')
+    item = models.CharField(max_length=64, default=None)
+    description = models.TextField(default=None)
+    starting_price = models.DecimalField(max_digits=10, decimal_places=2, default=None)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2, default=None)
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, default=None, related_name='buyer')
+    image = models.URLField(null=True, default=None)
+    active = True
+    category = []
+
+class Bids(models.Model):
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=None)
+    item = models.ForeignKey(Listings, on_delete=models.CASCADE, default=None)
+
+class Comments(models.Model):
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    comment = models.TextField(default=None)
+    item = models.ForeignKey(Listings, on_delete=models.CASCADE, default=None)
